f0119268a8d885c5c78a17a273e2be1ac85b8c69
Matthew O'Mara <tuh39725@temple.edu>
2022-01-26 14:36:59 -0500

    declare input variables var to val

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -9,25 +9,25 @@ fun main(args: Array<String>){
     val scanner = Scanner(System.`in`)
 
     print("Set Square.Length: ")
-    var l:Int = scanner.nextInt()
+    val l:Int = scanner.nextInt()
     print("Set Square.Height: ")
-    var h = scanner.nextInt()
+    val h = scanner.nextInt()
     mySquare.setDimensions(l.toDouble(), h.toDouble())
 
     print("Set Circle.Radius: ")
-    var r:Int = scanner.nextInt()
+    val r:Int = scanner.nextInt()
     myCircle.setDimensions(r.toDouble())
 
     print("Set Triangle.sideA: ")
-    var sideA:Int = scanner.nextInt()
+    val sideA:Int = scanner.nextInt()
     print("Set Triangle.sideB: ")
-    var sideB:Int = scanner.nextInt()
+    val sideB:Int = scanner.nextInt()
     print("Set Triangle.sideC: ")
-    var sideC:Int = scanner.nextInt()
+    val sideC:Int = scanner.nextInt()
     myTriangle.setDimensions(sideA.toDouble(), sideB.toDouble(), sideC.toDouble())
 
     print("Set EquilateralTriangle.side: ")
-    var side:Int = scanner.nextInt()
+    val side:Int = scanner.nextInt()
     myEquilateralTriangle.setDimensions(side.toDouble())
 
     println("\n" + mySquare.name)
