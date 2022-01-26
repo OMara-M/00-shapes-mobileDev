d0728d71b4b7d3d03289aeda23b5e933dad42046
Matthew O'Mara <tuh39725@temple.edu>
2022-01-26 00:54:39 -0500

    add main

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,19 @@
+import java.util.Scanner
+
 fun main(args: Array<String>){
-    println("hello World")
+    val mySquare = Square("Square")
+    val myCircle = Circle("Circle")
+    val myTriangle = Triangle("Triangle")
+    val myEquilateralTriangle = EquilateralTriangle("EquilateralTriangle")
+
+    val scanner = Scanner(System.`in`)
+
+    print("Set Square.Length: ")
+    var l:Int = scanner.nextInt()
+    print("Set Square.Height: ")
+    var h = scanner.nextInt()
+    mySquare.setDimensions(l.toDouble(), h.toDouble())
+    println(mySquare.name)
+    mySquare.printDimensions()
+    println(mySquare.getArea())
 }
